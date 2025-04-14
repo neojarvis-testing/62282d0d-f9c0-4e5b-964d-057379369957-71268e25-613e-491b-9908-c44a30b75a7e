84a27c5aa392c9987bb8854d663ca7cae6056854
mdsaif-masoom <saifmasoom121@gmail.com>
2025-04-14 05:56:17 +0000

    Saif Commit 11:26

--- Project/src/test/java/stepdefinition/Hooks.java
+++ Project/src/test/java/stepdefinition/Hooks.java
@@ -1,42 +1,43 @@
 package stepdefinition;
-
+ 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
-
 import io.cucumber.java.After;
 import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
 import io.cucumber.java.BeforeAll;
+import io.cucumber.java.Scenario;
 import utils.Base;
 import utils.ReportHelper;
-
+ 
 public class Hooks extends Base {
-
-    public static ExtentReports reports; 
     public static ExtentTest test;
-
-     @BeforeAll
-    public static void beforeA(){ 
-        reports=ReportHelper.createExtentReporter("Cleveland_Clinic");
-
+    public static ExtentReports reports;
+ 
+    @BeforeAll
+    public static void reportInitialization() {
+   
+        reports = ReportHelper.createExtentReporter("Cleveland Clinic");
+ 
     }
-
+ 
     @Before
-    public void before() { 
+    public void browserInitialization(Scenario sc) {
         openBrowser();
-        test=reports.createTest("Test Case 02-HomePage Map ");
-    
+        test=reports.createTest(sc.getName());
+ 
     }
-
+ 
     @After
-    public void after(){ 
+    public void browserTeardown() {
         driver.quit();
+ 
     }
-
+ 
     @AfterAll
-    public static void afterAll(){ 
+    public static void closeReportResources() {
         reports.flush();
+ 
     }
-    
+ 
 }
-    
