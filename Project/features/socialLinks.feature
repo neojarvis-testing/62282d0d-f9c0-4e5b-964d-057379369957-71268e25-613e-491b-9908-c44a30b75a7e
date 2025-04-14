273cb9b8150e91f3d7f119f195f90fd046fa1b07
HarshiTsTomar <harshitt59@gmail.com>
2025-04-14 06:25:50 +0000

    merge ready final

--- Project/features/socialLinks.feature
+++ Project/features/socialLinks.feature
@@ -3,37 +3,11 @@ Feature: Homepage Social Media Links
   I want to click on social media links on the homepage
   So that I can visit the respective social media pages
 
-  Scenario: Click on Facebook link
-    Given I am on the homepage
+  Scenario: Test Case 07 - Social Links
     When I click on the Facebook link
-    Then I should be redirected to the Facebook page
-
-  Scenario: Click on Twitter link
-    Given I am on the homepage
-    When I click on the Twitter link
-    Then I should be redirected to the Twitter page
-
-  Scenario: Click on YouTube link
-    Given I am on the homepage
-    When I click on the YouTube link
-    Then I should be redirected to the YouTube page
-
-  Scenario: Click on Instagram link
-    Given I am on the homepage
-    When I click on the Instagram link
-    Then I should be redirected to the Instagram page
-
-  Scenario: Click on LinkedIn link
-    Given I am on the homepage
-    When I click on the LinkedIn link
-    Then I should be redirected to the LinkedIn page
-
-  Scenario: Click on Pinterest link
-    Given I am on the homepage
-    When I click on the Pinterest link
-    Then I should be redirected to the Pinterest page
-
-  Scenario: Click on Snapchat link
-    Given I am on the homepage
-    When I click on the Snapchat link
-    Then I should be redirected to the Snapchat page
+    And I click on the Twitter link
+    And I click on the YouTube link
+    And I click on the Instagram link
+    And I click on the LinkedIn link
+    And I click on the Pinterest link
+    And I click on the Snapchat link
--- Project/src/test/java/stepdefinition/FooterActionSteps.java
+++ Project/src/test/java/stepdefinition/FooterActionSteps.java
@@ -1,124 +1,67 @@
 package stepdefinition;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
-import io.cucumber.java.en.Then;
-import org.junit.Assert;
 import pages.HomePageActions;
 
 public class FooterActionSteps {
 
     HomePageActions homepageActions = new HomePageActions();
 
-   
 
     @When("I click on the Appointments Access link")
     public void i_click_on_the_appointments_access_link() {
         homepageActions.clickAppointmentsAccess();
     }
 
-    @Then("I should be redirected to the Appointments Access page")
-    public void i_should_be_redirected_to_the_appointments_access_page() {
-        
-    }
-
-    @When("I click on the Accepted Insurance link")
+    @And("I click on the Accepted Insurance link")
     public void i_click_on_the_accepted_insurance_link() {
         homepageActions.clickAcceptedInsurance();
     }
 
-    @Then("I should be redirected to the Accepted Insurance page")
-    public void i_should_be_redirected_to_the_accepted_insurance_page() {
-        
-    }
-
-    @When("I click on the Events Calendar link")
+    @And("I click on the Events Calendar link")
     public void i_click_on_the_events_calendar_link() {
         homepageActions.clickEventsCalender();
     }
 
-    @Then("I should be redirected to the Events Calendar page")
-    public void i_should_be_redirected_to_the_events_calendar_page() {
-        
-    }
-
-    @When("I click on the Financial Assistance link")
+    @And("I click on the Financial Assistance link")
     public void i_click_on_the_financial_assistance_link() {
         homepageActions.clickFinancialAssistance();
     }
 
-    @Then("I should be redirected to the Financial Assistance page")
-    public void i_should_be_redirected_to_the_financial_assistance_page() {
-        
-    }
-
-    @When("I click on the Give to Cleveland Clinic link")
+    @And("I click on the Give to Cleveland Clinic link")
     public void i_click_on_the_give_to_cleveland_clinic_link() {
         homepageActions.clickGiveCleveland();
     }
 
-    @Then("I should be redirected to the Give to Cleveland Clinic page")
-    public void i_should_be_redirected_to_the_give_to_cleveland_clinic_page() {
-        
-    }
-
-    @When("I click on the Pay Bill link")
+    @And("I click on the Pay Bill link")
     public void i_click_on_the_pay_bill_link() {
         homepageActions.clickPayBill();
     }
 
-    @Then("I should be redirected to the Pay Bill page")
-    public void i_should_be_redirected_to_the_pay_bill_page() {
-        
-    }
-
-    @When("I click on the Price Transparency link")
+    @And("I click on the Price Transparency link")
     public void i_click_on_the_price_transparency_link() {
         homepageActions.clickPriceTransparency();
     }
 
-    @Then("I should be redirected to the Price Transparency page")
-    public void i_should_be_redirected_to_the_price_transparency_page() {
-        
-    }
-
-    @When("I click on the Refer Patient link")
+    @And("I click on the Refer Patient link")
     public void i_click_on_the_refer_patient_link() {
         homepageActions.clickReferPatient();
     }
 
-    @Then("I should be redirected to the Refer Patient page")
-    public void i_should_be_redirected_to_the_refer_patient_page() {
-        
-    }
-
-    @When("I click on the Phone Directory link")
+    @And("I click on the Phone Directory link")
     public void i_click_on_the_phone_directory_link() {
         homepageActions.clickPhoneDirectory();
     }
 
-    @Then("I should be redirected to the Phone Directory page")
-    public void i_should_be_redirected_to_the_phone_directory_page() {
-        
-    }
-
-    @When("I click on the Virtual Opinions link")
+    @And("I click on the Virtual Opinions link")
     public void i_click_on_the_virtual_opinions_link() {
         homepageActions.clickVirtualOpinions();
     }
 
-    @Then("I should be redirected to the Virtual Opinions page")
-    public void i_should_be_redirected_to_the_virtual_opinions_page() {
-        
-    }
-
-    @When("I click on the Virtual Visits link")
+    @And("I click on the Virtual Visits link")
     public void i_click_on_the_virtual_visits_link() {
         homepageActions.clickVirtualVisits();
-    }
-
-    @Then("I should be redirected to the Virtual Visits page")
-    public void i_should_be_redirected_to_the_virtual_visits_page() {
-        
     }
 }
