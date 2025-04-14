27b98868efc3f864decc63c8c4b7b5a1d1ecd171
Kiruthik7 <kiruthikvijeyraja@gmail.com>
2025-04-14 07:07:52 +0000

    BDD-Kiruthik Final Update

--- Project/src/test/java/runner/TestRun.java
+++ Project/src/test/java/runner/TestRun.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
  
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "./features", glue="stepdefinition",tags = "@doctor")
+@CucumberOptions(features = "./features", glue="stepdefinition")
 public class TestRun {
    
 }
