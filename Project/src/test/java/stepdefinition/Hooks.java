317b9ef7dc6f46f4465745199caff7f980d06e99
RewaPawar25 <rewapawar25@gmail.com>
2025-04-14 06:46:09 +0000

    BDD-Rewa

--- Project/src/test/java/stepdefinition/Hooks.java
+++ Project/src/test/java/stepdefinition/Hooks.java
@@ -1,9 +1,7 @@
 package stepdefinition;
-
-
+ 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
-
 import io.cucumber.java.After;
 import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
@@ -11,34 +9,35 @@ import io.cucumber.java.BeforeAll;
 import io.cucumber.java.Scenario;
 import utils.Base;
 import utils.ReportHelper;
-
-public class Hooks extends Base{
-
+ 
+public class Hooks extends Base {
     public static ExtentTest test;
     public static ExtentReports reports;
-
+ 
     @BeforeAll
-    public static void reportConfig(){
+    public static void reportInitialization() {
+   
         reports = ReportHelper.createExtentReporter("Cleveland Clinic");
+ 
     }
  
     @Before
-    public void initConfig(Scenario sc){
-        test = reports.createTest(sc.getName());
+    public void browserInitialization(Scenario sc) {
         openBrowser();
+        test=reports.createTest(sc.getName());
+ 
     }
  
-    
- 
     @After
-    public void endConfig(){
-        if(driver!=null);
+    public void browserTeardown() {
         driver.quit();
+ 
     }
  
     @AfterAll
-    public static void reportEnd(){
+    public static void closeReportResources() {
         reports.flush();
+ 
     }
-
+ 
 }
